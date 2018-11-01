48fe61a957b0427eba838a2147d13a459aca3e81
Ayshan Safarova <aysensafarova@gmail.com>
2018-11-01 04:35:25 +0400

    login/register/header-dropdown/toast changing

--- Bikely/assets/snippets/pages/user/login.js
+++ Bikely/assets/snippets/pages/user/login.js
@@ -1,19 +1,19 @@
-//== Class Definition
+﻿//== Class Definition
 var SnippetLogin = function() {
 
     var login = $('#m_login');
 
-    var showErrorMsg = function(form, type, msg) {
-        var alert = $('<div class="m-alert m-alert--outline alert alert-' + type + ' alert-dismissible" role="alert">\
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>\
-			<span></span>\
-		</div>');
+  //  var showErrorMsg = function(form, type, msg) {
+  //      var alert = $('<div class="m-alert m-alert--outline alert alert-' + type + ' alert-dismissible" role="alert">\
+		//	<button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>\
+		//	<span></span>\
+		//</div>');
 
-        form.find('.alert').remove();
-        alert.prependTo(form);
-        alert.animateClass('fadeIn animated');
-        alert.find('span').html(msg);
-    }
+  //      form.find('.alert').remove();
+  //      alert.prependTo(form);
+  //      alert.animateClass('fadeIn animated');
+  //      alert.find('span').html(msg);
+  //  }
 
     //== Private Functions
 
@@ -63,151 +63,151 @@ var SnippetLogin = function() {
         });
     }
 
-    var handleSignInFormSubmit = function() {
-        $('#m_login_signin_submit').click(function(e) {
-            e.preventDefault();
-            var btn = $(this);
-            var form = $(this).closest('form');
+    //var handleSignInFormSubmit = function() {
+    //    $('#m_login_signin_submit').click(function(e) {
+    //        e.preventDefault();
+    //        var btn = $(this);
+    //        var form = $(this).closest('form');
 
-            form.validate({
-                rules: {
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    password: {
-                        required: true
-                    }
-                }
-            });
+    //        form.validate({
+    //            rules: {
+    //                username: {
+    //                    required: true,
+    //                    email: true
+    //                },
+    //                password: {
+    //                    required: true
+    //                }
+    //            }
+    //        });
 
-            if (!form.valid()) {
-                return;
-            }
+    //        if (!form.valid()) {
+    //            return;
+    //        }
 
-            btn.addClass('m-loader m-loader--right m-loader--light').attr('disabled', true);
+    //        btn.addClass('m-loader m-loader--right m-loader--light').attr('disabled', true);
 
-            form.ajaxSubmit({
-                url: '',
-                success: function(response, status, xhr, $form) {
-                	// similate 2s delay
-                	setTimeout(function() {
-	                    btn.removeClass('m-loader m-loader--right m-loader--light').attr('disabled', false);
-	                    showErrorMsg(form, 'danger', 'Incorrect username or password. Please try again.');
-                    }, 2000);
-                }
-            });
-        });
-    }
+    //        form.ajaxSubmit({
+    //            url: '',
+    //            success: function(response, status, xhr, $form) {
+    //            	// similate 2s delay
+    //            	setTimeout(function() {
+	   //                 btn.removeClass('m-loader m-loader--right m-loader--light').attr('disabled', false);
+	   //                 showErrorMsg(form, 'danger', 'Yanlış istifadəçi adı və ya parol');
+    //                }, 2000);
+    //            }
+    //        });
+    //    });
+    //}
 
-    var handleSignUpFormSubmit = function() {
-        $('#m_login_signup_submit').click(function(e) {
-            e.preventDefault();
+    //var handleSignUpFormSubmit = function() {
+    //    $('#m_login_signup_submit').click(function(e) {
+    //        e.preventDefault();
 
-            var btn = $(this);
-            var form = $(this).closest('form');
+    //        var btn = $(this);
+    //        var form = $(this).closest('form');
 
-            form.validate({
-                rules: {
-                    fullname: {
-                        required: true
-                    },
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    password: {
-                        required: true
-                    },
-                    rpassword: {
-                        required: true
-                    },
-                    agree: {
-                        required: true
-                    }
-                }
-            });
+    //        form.validate({
+    //            rules: {
+    //                fullname: {
+    //                    required: true
+    //                },
+    //                email: {
+    //                    required: true,
+    //                    email: true
+    //                },
+    //                password: {
+    //                    required: true
+    //                },
+    //                rpassword: {
+    //                    required: true
+    //                },
+    //                agree: {
+    //                    required: true
+    //                }
+    //            }
+    //        });
 
-            if (!form.valid()) {
-                return;
-            }
+    //        if (!form.valid()) {
+    //            return;
+    //        }
 
-            btn.addClass('m-loader m-loader--right m-loader--light').attr('disabled', true);
+    //        btn.addClass('m-loader m-loader--right m-loader--light').attr('disabled', true);
 
-            form.ajaxSubmit({
-                url: '',
-                success: function(response, status, xhr, $form) {
-                	// similate 2s delay
-                	setTimeout(function() {
-	                    btn.removeClass('m-loader m-loader--right m-loader--light').attr('disabled', false);
-	                    form.clearForm();
-	                    form.validate().resetForm();
+    //        form.ajaxSubmit({
+    //            url: '',
+    //            success: function(response, status, xhr, $form) {
+    //            	// similate 2s delay
+    //            	setTimeout(function() {
+	   //                 btn.removeClass('m-loader m-loader--right m-loader--light').attr('disabled', false);
+	   //                 form.clearForm();
+	   //                 form.validate().resetForm();
 
-	                    // display signup form
-	                    displaySignInForm();
-	                    var signInForm = login.find('.m-login__signin form');
-	                    signInForm.clearForm();
-	                    signInForm.validate().resetForm();
+	   //                 // display signup form
+	   //                 displaySignInForm();
+	   //                 var signInForm = login.find('.m-login__signin form');
+	   //                 signInForm.clearForm();
+	   //                 signInForm.validate().resetForm();
 
-	                    showErrorMsg(signInForm, 'success', 'Thank you. To complete your registration please check your email.');
-	                }, 2000);
-                }
-            });
-        });
-    }
+	   //                 showErrorMsg(signInForm, 'success', 'Təbriklər! Zəhmət olmasa, qeydiyyati tamamlamaq üçün email adresinize baş çəkin');
+	   //             }, 2000);
+    //            }
+    //        });
+    //    });
+    //}
 
-    var handleForgetPasswordFormSubmit = function() {
-        $('#m_login_forget_password_submit').click(function(e) {
-            e.preventDefault();
+    //var handleForgetPasswordFormSubmit = function() {
+    //    $('#m_login_forget_password_submit').click(function(e) {
+    //        e.preventDefault();
 
-            var btn = $(this);
-            var form = $(this).closest('form');
+    //        var btn = $(this);
+    //        var form = $(this).closest('form');
 
-            form.validate({
-                rules: {
-                    email: {
-                        required: true,
-                        email: true
-                    }
-                }
-            });
+    //        form.validate({
+    //            rules: {
+    //                email: {
+    //                    required: true,
+    //                    email: true
+    //                }
+    //            }
+    //        });
 
-            if (!form.valid()) {
-                return;
-            }
+    //        if (!form.valid()) {
+    //            return;
+    //        }
 
-            btn.addClass('m-loader m-loader--right m-loader--light').attr('disabled', true);
+    //        btn.addClass('m-loader m-loader--right m-loader--light').attr('disabled', true);
 
-            form.ajaxSubmit({
-                url: '',
-                success: function(response, status, xhr, $form) { 
-                	// similate 2s delay
-                	setTimeout(function() {
-                		btn.removeClass('m-loader m-loader--right m-loader--light').attr('disabled', false); // remove 
-	                    form.clearForm(); // clear form
-	                    form.validate().resetForm(); // reset validation states
+    //        form.ajaxSubmit({
+    //            url: '',
+    //            success: function(response, status, xhr, $form) { 
+    //            	// similate 2s delay
+    //            	setTimeout(function() {
+    //            		btn.removeClass('m-loader m-loader--right m-loader--light').attr('disabled', false); // remove 
+	   //                 form.clearForm(); // clear form
+	   //                 form.validate().resetForm(); // reset validation states
 
-	                    // display signup form
-	                    displaySignInForm();
-	                    var signInForm = login.find('.m-login__signin form');
-	                    signInForm.clearForm();
-	                    signInForm.validate().resetForm();
+	   //                 // display signup form
+	   //                 displaySignInForm();
+	   //                 var signInForm = login.find('.m-login__signin form');
+	   //                 signInForm.clearForm();
+	   //                 signInForm.validate().resetForm();
 
-	                    showErrorMsg(signInForm, 'success', 'Cool! Password recovery instruction has been sent to your email.');
-                	}, 2000);
-                }
-            });
-        });
-    }
+	   //                 showErrorMsg(signInForm, 'success', 'Email adresinizə link göndərilmişdir');
+    //            	}, 2000);
+    //            }
+    //        });
+    //    });
+    //}
 
     //== Public Functions
     return {
         // public functions
         init: function() {
             handleFormSwitch();
-            handleSignInFormSubmit();
-            handleSignUpFormSubmit();
-            handleForgetPasswordFormSubmit();
+            //handleSignInFormSubmit();
+            //handleSignUpFormSubmit();
+            //handleForgetPasswordFormSubmit();
         }
     };
 }();
